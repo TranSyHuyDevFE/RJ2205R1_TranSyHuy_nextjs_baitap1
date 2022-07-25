cd8fcb51f5bd27f1fa1e6d5e9516489affb0adf4
TranSyHuy <syhuy01072002@gmail.com>
2022-07-25 18:02:53 +0700

    bai thuc hanh 1 va 2, bai tap form login

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Layout from "../components/layout";
-
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import { getStudents } from "./mock-data/data";
 export default function Home() {
   return (
     <Layout>
@@ -9,6 +11,28 @@ export default function Home() {
         <meta name="description" content="homepage" />
       </Head>
       <h1>Home Page</h1>
+      <table className={styles.table}>
+        <thead>
+          <tr className={styles.tr}>
+            <th className={styles.th}>ID</th>
+            <th className={styles.th}>Name</th>
+            <th className={styles.th}>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {getStudents().map((student) => (
+            <tr className={styles.tr} key={student.id}>
+              <td className={styles.td}>{student.id}</td>
+              <td className={styles.td}>{student.name}</td>
+              <td className={styles.td}>
+                <Link href={`/posts/${encodeURIComponent(student.id)}`}>
+                  <a>Show</a>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
